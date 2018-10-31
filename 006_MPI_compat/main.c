14b0d2c0e3674d1b8403268c2fe0ac4be6092852
Vincent Heusinkveld <vincenth@shell-2.grs.vrlab.tudelft.nl>
2018-10-31 11:23:03 +0100

    Test runs for differs for different configurations of MPI and OpenMP

---
+++ 006_MPI_compat/main.c
@@ -0,0 +1,77 @@
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "grid/octree.h" // For 3D
+#include "view.h"
+#include "navier-stokes/centered.h"
+#include "tracer.h"
+#include "diffusion.h"
+
+/* Global variables */
+int minlevel, maxlevel;         // Grid depths
+double meps, eps;		// Maximum error and error in u fields
+
+char sim_ID[] = "single";
+
+#include "physics.h"
+#include "fan.h"
+#include "diagnostics.h"
+
+/* Initialisation */
+int main() {
+	for (maxlevel = 9; maxlevel < 10; maxlevel++){
+    	init_grid(2<<5);
+   	L0 = 50.;
+   	X0 = Y0 = Z0 = 0.;
+	a = av; 
+
+	u.x.refine = refine_linear; 
+	u.y.refine = refine_linear;
+	#if dimension == 3
+		u.z.refine = refine_linear;
+	#endif
+	fan.prolongation = fraction_refine;
+	p.refine = p.prolongation = refine_linear;
+	b.gradient = minmod2; // Flux limiter 
+
+  	minlevel = 3; 
+  	//maxlevel = 8;
+  	meps = 10.;
+	DT = 10E-5;
+        TOLERANCE=10E-6;
+	CFL = 0.5;
+	
+	sim_dir_create();
+	out.sim_i++;
+    	run();
+	}
+}
+
+/* Initialisation */
+event init(t = 0){
+	init_physics();
+	init_rotor();
+	fan.prolongation=fraction_refine;
+	refine (fan[] > 0. && level < maxlevel);
+	eps = min(meps, 0.085*rot.cu);
+}
+
+event init_change(i=10){
+	TOLERANCE=10E-3;
+	DT = 0.05;
+}
+
+/* Adaptivity function called */
+event adapt(i++) {
+	adapt_wavelet((scalar *){fan, u},(double []){0.,eps,eps,eps},maxlevel,minlevel);
+}
+
+/* Progress event */
+event progress(t+=2.) {
+	fprintf(stderr, "i=%d t=%g p=%d u=%d b=%d \n", i, t, mgp.i, mgu.i, mgb.i);
+}
+
+event end(t=60){
+}
