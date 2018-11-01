3c30b84a267d3361fafcba2175015558799b70fe
VHeusinkveld <vincent.heusinkveld@gmail.com>
2018-11-01 10:53:38 +0100

    Data_processing

--- 000_data_processing/Data_profiles.py
+++ 000_data_processing/Data_profiles.py
@@ -1,34 +1,68 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from os import listdir
+font = {'family' : 'serif',
+        'weight' : 'normal',
+        'size'   : 18}
+
+plt.rc('font', **font)
+
 
 def one2two(data):
     data_points = int(np.sqrt(len(data)))
     return np.reshape(np.array(data),(data_points, data_points))        
+def arr(data):
+    return np.array(data)
+
+exp_name = "010_MPI_compat"
+res_name = "results"
+cas_name = "single"
+try_name = "00/"
+
+data_path_root = "../" + exp_name +"/"+ res_name +"/"+ cas_name+try_name 
 
 #%%
-data_path = "../005_MPI_compat/results/single00/profiles"
+data_path = data_path_root + "profiles"
 data_files = listdir(data_path)
 legend = []
+fig = plt.figure(figsize=[8,5], dpi=72)
 for file in data_files:
     time =  float(file[file.find("t=")+2:])
-    if (time%20==0):
+    if (time%10==0 and time > 0):
         legend.append(file)
         data = pd.read_csv(data_path+"/"+file, "\t")
         plt.plot(30*data['bdiff'], data['y'])
-        print(data)
-#b_ref = 0.5*data['y']*9.81/273;
-#plt.plot(b_ref, data['y'])
-#plt.xlim([0.25, 0.55])
-plt.xlim([-0.2, 0.2])
-plt.ylim([10, 35])
+        
+plt.xlabel('dT [K]')
+plt.ylabel('height [m]')
+plt.ylim([0, 50])
 plt.legend(legend)
-plt.show()
+plt.tight_layout()
+plt.savefig(data_path_root + "profile_buoyancy.png")
+#%%
+
+data_path = data_path_root
+data = pd.read_csv(data_path_root + "/output", "\t")
+
+def my_fun(x):
+    return x[-1] - x[0]
+
+fig = plt.figure(figsize=[8,5], dpi=72)
+plt.plot(data['t'], data['Work'])
+plt.plot(data['t'], data['Ekin'])
+plt.plot(data['t'], -data['bE'])
+plt.plot(data['t'], data['Ekin']-data['bE'])
+plt.legend(['Work', 'Ekin', 'bE', 'Total energy'])
+plt.xlabel('time [s]')
+plt.ylabel('Energy [J]')
+plt.tight_layout()
+plt.savefig(data_path_root + "energy.png")
 
 #%%
 from matplotlib import animation
-data_path = "../005_MPI_compat/results/single00/slices"
+data_path = data_path_root + "slices"
 data_files = listdir(data_path)
 files_length = len(data_files)
 
@@ -42,14 +76,15 @@ def animate(i):
     img = plt.contourf(one2two(data['#']), one2two(data['2:y']), one2two(data['3:z']))
     plt.colorbar()
     plt.title(file)
+    plt.tight_layout()
     
     return img
 
-fig = plt.figure()
+fig = plt.figure(figsize=[8,5], dpi=72)
 Nt = files_length
 anim = animation.FuncAnimation(fig, animate, frames=Nt)
 
-anim.save('animation.mp4', fps=1)
+anim.save(data_path_root + 'animation.mp4', fps=5)
 
 '''
 from mpl_toolkits.mplot3d import Axes3D
