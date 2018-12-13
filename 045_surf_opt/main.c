b560852cc6c51fb4d55788ae2940da7476b85541
Vincent Heusinkveld <vincenth@shell-1.grs.vrlab.tudelft.nl>
2018-12-13 12:09:37 +0100

    new runs advection, and lumped paraemter

---
+++ 045_surf_opt/main.c
@@ -0,0 +1,97 @@
+/** Include required libraries */ 
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "grid/octree.h" 		// For 3D
+#include "view.h"			// For bview
+#include "navier-stokes/centered.h"     // Navier stokes 
+#include "tracer.h"			// Tracers
+#include "diffusion.h"			// Diffusion 
+
+/** Global variables */
+int minlevel, maxlevel;         	// Grid depths
+double meps, eps;			// Maximum error and error in u fields
+double TEND = 300.;
+
+char sim_ID[] = "rotation";		// Simulation identifier
+char sim_var[] = "theta";  		// Notes if a variable is varied over runs
+
+#include "physics.h"			// Physics of the simulation 
+#include "fan.h"			// Include a fan
+#include "diagnostics.h"		// Perform diagnostics
+
+/** Initialisation */
+int main() {	
+    minlevel = 5;
+    maxlevel = 9;
+
+    L0 = 200.;
+    X0 = Y0 = Z0 = 0.;
+
+    // Possibility to run for variable changes
+    for(rot.theta=100.*M_PI/180.; rot.theta<=151.*M_PI/180.; rot.theta+=100.*M_PI/180.) {
+        init_grid(1<<6);
+	a = av; 
+
+        foreach_dimension() {
+	    u.x.refine = refine_linear;  		// Momentum conserved 
+	}
+
+	fan.prolongation = fraction_refine;		// Fan is a volume fraction
+	p.refine = p.prolongation = refine_linear;
+	b.gradient = minmod2; 				// Flux limiter 
+
+  	meps = 10.;					// Maximum adaptivity criterion
+	DT = 10E-5;					// For poisson solver 
+        TOLERANCE=10E-6;				// For poisson solver 
+	CFL = 0.5;					// CFL condition
+
+	sim_dir_create();				// Create relevant dir's
+	out.sim_i++;					// Simulation iteration
+ 
+    	run();						// Start simulation 
+
+    }
+}
+
+/** Initialisation */
+event init(t=0) {
+    rot.rotate = true;
+    rot.phi = 0;
+    eps = .7; //min(meps, 10.);
+    
+    init_physics();
+    init_rotor();
+
+    fan.prolongation=fraction_refine;
+    refine(fan[] > 0. && level < maxlevel);
+
+}
+
+/** Return to standard tolerances and DTs for poisson solver */ 
+event init_change(i=10) {
+    TOLERANCE=10E-3;
+    DT = 0.5;
+}
+
+/** Adaptivity */
+event adapt(i++) {
+    adapt_wavelet((scalar *){fan,u,b},(double []){0.,eps,eps,eps,3.*9.81/273},maxlevel,minlevel);
+}
+
+/** Progress event */
+event progress(t+=5) {
+    fprintf(stderr, "i=%d t=%g p=%d u=%d b=%d \n", i, t, mgp.i, mgu.i, mgb.i);
+}
+
+event dumpfields(t=60; t+=60) {
+    char nameDump[90];
+    snprintf(nameDump, 90, "./%s/fielddump", out.dir);
+    dump(file = nameDump, list = all);
+}
+
+/** End the simulation */
+event end(t=TEND) {
+}
---
+++ 045_surf_opt/physics.h
@@ -0,0 +1,123 @@
+#if dimension == 3
+	#include "SGS.h"
+#endif 
+
+#define CP 1005.	// C_p for air 
+#define gCONST 9.81	// Gravitational constant
+#define TREF 273.	// Kelvin
+#define INVERSION .2 	// Kelvin per meter
+
+//#define STRAT(s) gCONST*(INVERSION + gCONST/CP)*s/TREF 	// Stratification 
+#define WIND(s) def.wind/0.41*log((s-0.075)/0.1) 	// log Wind profile TODO
+
+//#define strat(s) gCONST*5.*log(sqrt(s)+1)/TREF
+
+#define QFLX (-0.005)
+#define BSURF (1.5*b[] - 0.5*b[0, 1])
+#define GFLX (-Lambda*(BSURF - bd))
+double bd = 0, Lambda = 0.0125;
+#define STRAT(s) log(s + a1 + 1.) - log(a1 + 1.) + (QFLX/Lambda + bd)
+double a1 = 1;
+
+
+double crho = 1.;
+scalar b[];
+scalar * tracers = {b};
+	
+face vector av[]; 
+struct sCase def;
+
+struct sCase {
+	double wind;
+	double wphi;
+};
+
+void init_physics(){
+ 	def.wind = -0.2;
+        def.wphi = 0.;
+
+	b.nodump = false; // TODO
+
+	if(def.wind == 0.){    	
+	    u.n[bottom] = dirichlet(0.);
+	    u.t[bottom] = dirichlet(0.);
+
+	    u.n[top] = dirichlet(0.);
+ 	    u.t[top] = neumann(0.);
+
+            periodic (left);
+
+	} else if(fabs(def.wind) > 0.) {
+	    //if(def.wind > 0.) {
+	    //} else if(def.wind<0.) {
+	    //}
+
+            u.n[right] = dirichlet(WIND(y));
+
+	    u.n[left] = dirichlet(WIND(y));
+	    
+	    u.n[bottom] = dirichlet(0.);
+	    u.t[bottom] = dirichlet(0.); //neumann(0.);
+
+	    u.n[top] = dirichlet(0.);
+ 	    u.t[top] = neumann(0.);
+
+            b[left] = dirichlet(STRAT(y));
+            b[right] = dirichlet(STRAT(y));
+        }
+	
+	
+	b[bottom] = BSURF;
+	b[top] = dirichlet(STRAT(y));
+
+	#if dimension == 3
+		u.r[bottom] = dirichlet(0.); //	neumann(0.);
+		u.r[top] = neumann(0.); 
+		
+		//Evis[bottom] = dirichlet(0.);
+        	//Evis[top] = dirichlet(0.);
+
+		periodic(front);
+	#endif  
+	foreach() {
+		b[] = STRAT(y);
+       	        u.x[] = WIND(y);
+	}
+
+    while(adapt_wavelet((scalar *){u,b},(double []){eps,eps,eps,3.*9.81/273},maxlevel,minlevel).nf) {
+	foreach() {
+		b[] = STRAT(y);
+       	        u.x[] = WIND(y);
+	}
+    }
+
+
+}
+
+/* Gravity forcing */
+event acceleration(i++){
+	foreach_face(y){
+		av.y[] = (b[] + b[0,-1])/2.;
+	}
+}
+
+mgstats mgb;
+
+/* Diffusion */
+event tracer_diffusion(i++){
+    scalar r[];
+    foreach() {
+        r[] = 0;
+        if (y < Delta)
+            r[] = (QFLX + GFLX)/Delta;
+     }
+    double flx = 0, bt = 0;
+    foreach_boundary(bottom){
+        flx += (QFLX + GFLX) * Delta;
+         bt += BSURF * Delta;
+    }
+    bt /= L0;
+    flx /= L0;
+    printf("%g %g %g %d\n", t, flx, bt, i);  
+    mgb = diffusion(b, dt, mu, r = r);
+}
